85ba36e5207d8001c9ae61178d3653f273c4fb35
Moises Junior <m.junior@eq.ufrj.br>
2021-11-05 11:15:45 -0300

    Distribuição de cartas feita

--- parrot.js
+++ parrot.js
@@ -1,14 +1,14 @@
 let numeroDeCartas = prompt("Qual é o número de cartas desejado?");
-// let cartaEscolhida = [];
-// let nomeCartas = [
-// bobross,
-// explody,
-// metal,
-// revertit,
-// triplets,
-// unicorn,
-// fiesta
-// ] 
+let cartaEscolhida = [];
+let nomeCartas = [
+"bobross",
+"explody",
+"metal",
+"revertit",
+"triplets",
+"unicorn",
+"fiesta"
+] 
 
 while (numeroDeCartas < 4 || numeroDeCartas > 14 || numeroDeCartas % 2 !== 0) {
     numeroDeCartas = prompt("Qual é o número desejado de cartas?")
@@ -17,26 +17,33 @@ while (numeroDeCartas < 4 || numeroDeCartas > 14 || numeroDeCartas % 2 !== 0) {
 lancarCartas ();
 
 function lancarCartas () {
-    // embaralhando()
+    embaralhando()
     cartas = document.querySelector(".cartas")
 
+    for (let i = 0; i < (numeroDeCartas/2); i++) {
+        cartaEscolhida.push(nomeCartas[i]);
+        cartaEscolhida.push(nomeCartas[i]);
+    }
+    
+    cartaEscolhida.sort(comparador);
+
     for (let i = 0; i < numeroDeCartas; i++) {
-        cartas.innerHTML += "BRABOS"
-        // `<div class="carta" data-identifier="card">
-        //     <div class="carta-frente face" data-identifier="front-face">
-        //         <div class="imagem"></div>
-        //     </div>
-        //     <div class="carta-verso face" data-identifier="back-face">
-        //         <div><img src="assets/${cartaEscolhida[i]}parrot.gif"></div>
-        //     </div>
-        // </div>`
+        cartas.innerHTML +=
+        `<div class="carta" data-identifier="card">
+            <div class="carta-frente face" data-identifier="front-face">
+                <div class="imagem"></div>
+            </div>
+            <div class="carta-verso face" data-identifier="back-face">
+                <div><img src="assets/${cartaEscolhida[i]}parrot.gif"></div>
+            </div>
+        </div>`
     }
 }
 
-// function embaralhando () {
-//     cartaEscolhida = nomeCartas.sort(comparador);
-// }
+function embaralhando () {
+    nomeCartas.sort(comparador);
+}
 
-// function comparador () {
-//     return Math.random() - 0.5;
-// }
+function comparador () {
+    return Math.random() - 0.5;
+}
