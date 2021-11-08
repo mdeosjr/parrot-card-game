5390585822a56b4b85970edc004cc867efd27d20
Moises Junior <m.junior@eq.ufrj.br>
2021-11-08 12:21:49 -0300

    Projeto com alerta sem funcionar

--- parrot.js
+++ parrot.js
@@ -1,5 +1,5 @@
-let numeroDeCartas = prompt("Qual é o número de cartas desejado?");
-let cartaEscolhida = [];
+let numeroDeCartas = parseInt(prompt("Qual é o número de cartas desejado?"));
+let cartasSorteadas = [];
 let nomeCartas = [
 "bobross",
 "explody",
@@ -9,6 +9,11 @@ let nomeCartas = [
 "unicorn",
 "fiesta"
 ] 
+let carta1 = null; 
+let carta2 = null;
+let cartaEscolhida = 0;
+let qteJogadas = 0;
+const qteTotal = document.querySelectorAll(".carta-Selecionada").length;
 
 while (numeroDeCartas < 4 || numeroDeCartas > 14 || numeroDeCartas % 2 !== 0) {
     numeroDeCartas = prompt("Qual é o número desejado de cartas?")
@@ -16,34 +21,79 @@ while (numeroDeCartas < 4 || numeroDeCartas > 14 || numeroDeCartas % 2 !== 0) {
 
 lancarCartas ();
 
-function lancarCartas () {
-    embaralhando()
-    cartas = document.querySelector(".cartas")
-
-    for (let i = 0; i < (numeroDeCartas/2); i++) {
-        cartaEscolhida.push(nomeCartas[i]);
-        cartaEscolhida.push(nomeCartas[i]);
-    }
-    
-    cartaEscolhida.sort(comparador);
-
-    for (let i = 0; i < numeroDeCartas; i++) {
-        cartas.innerHTML +=
-        `<div class="carta" data-identifier="card">
-            <div class="carta-frente face" data-identifier="front-face">
-                <div class="imagem"></div>
-            </div>
-            <div class="carta-verso face" data-identifier="back-face">
-                <div><img src="assets/${cartaEscolhida[i]}parrot.gif"></div>
-            </div>
-        </div>`
-    }
+function comparador () {
+    return Math.random() - 0.5;
 }
 
 function embaralhando () {
     nomeCartas.sort(comparador);
 }
 
-function comparador () {
-    return Math.random() - 0.5;
+function lancarCartas () {
+    embaralhando()
+    let cartas = document.querySelector(".cartas")
+
+    if (numeroDeCartas === 8) {
+        cartas.style.width = "700px"
+    } else if (numeroDeCartas === 10) {
+        cartas.style.width = "800px"
+    } else if (numeroDeCartas === 14) {
+        cartas.style.width = "1100px"
+    }
+
+    for (let i = 0; i < (numeroDeCartas/2); i++) {
+        cartasSorteadas.push(nomeCartas[i]);
+        cartasSorteadas.push(nomeCartas[i]);
+        cartasSorteadas.sort(comparador);
+    }
+    
+    cartasSorteadas.sort(comparador);
+
+    for (let i = 0; i < numeroDeCartas; i++) {
+        cartas.innerHTML +=
+        `<div class="carta" data-identifier="card" onclick="cartaVirada(this)">
+            <div  class="carta-frente face" data-identifier="front-face">
+                <img src="assets/front.png">
+            </div>
+            <div class="carta-verso face" data-identifier="back-face">   
+                <img src="assets/${cartasSorteadas[i]}parrot.gif">
+            </div>        
+        </div>`
+    }
+}
+
+function cartaVirada (carta) {
+    carta.classList.add("carta-Selecionada"); 
+    cartaEscolhida++;
+
+    if (cartaEscolhida === 1) {
+        carta1 = carta;
+        qteJogadas++;
+    }
+
+    if (cartaEscolhida === 2) {
+        carta2 = carta;
+        qteJogadas++;
+
+        if (carta1.innerHTML === carta2.innerHTML) {
+            cartaEscolhida = 0;
+            carta1.removeAttribute("onclick");
+            carta2.removeAttribute("onclick");
+        } else {
+            setTimeout (delay, 1000);
+            cartaEscolhida = 0;
+        }
+    }
+    finalizar();
+}
+
+function delay () {
+    carta1.classList.remove("carta-Selecionada");
+    carta2.classList.remove("carta-Selecionada");
+}
+
+function finalizar () {
+    if (qteTotal === numeroDeCartas) {
+        alert("Você venceu");
+    }
 }
